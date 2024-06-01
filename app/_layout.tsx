96ca678dbb98f50419cbfc018a4ddf358a324d6a
gilberthappi <gdushimimana6@gmail.com>
2024-06-01 06:30:22 +0200

    Update files for login and signup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,7 +25,9 @@ const RootLayout = () => {
   <>
    <Stack>
     <Stack.Screen name='index' options={{ headerShown: false }} />
-    <Stack.Screen name='(auth)' options={{ headerShown: false }} />
+    <Stack.Screen name='login' options={{ headerShown: false }} />
+    <Stack.Screen name='sign-up' options={{ headerShown: false }} />
+    <Stack.Screen name='home' options={{ headerShown: false }} />
    </Stack>
    <StatusBar style='dark' />
   </>
