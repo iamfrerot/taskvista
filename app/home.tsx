825edd36104fcfcf8ac2a355f034b640a509e091
gilberthappi <gdushimimana6@gmail.com>
2024-06-01 13:57:59 +0200

    Update to login and signup

--- app/home.tsx
+++ app/home.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { router } from 'expo-router';
 
 const Home = () => {
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
-      <Text className="font-obold text-4xl">Welcome to TaskVista!</Text>
-    </SafeAreaView>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Screen</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-around', width: '100%' }}>
+        <TouchableOpacity onPress={() => router.push("/home")}>
+          <Text>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push("/tasks")}>
+          <Text>Tasks</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push("/inbox")}>
+          <Text>Inbox</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push("/search")}>
+          <Text>Search</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => router.push("/account")}>
+          <Text>Account</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
